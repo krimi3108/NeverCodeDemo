0da8b5fe8cd43fdc235b0cedcec2d2ef655d851d
Kritika Middha <kritika.middha@ranosys.com>
2019-03-12 13:01:34 +0530

    Initial Comit

---
+++ NeverCodeDemo/NeverCodeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeverCodeDemo
+//
+//  Created by Kritika Middha on 12/03/19.
+//  Copyright © 2019 Ranosys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
